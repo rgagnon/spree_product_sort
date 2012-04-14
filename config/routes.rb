d45fa9428fc88537ef42af11b8e70c5a3ff71fb0
dp6ai <patrickjdavenport@gmail.com>
2012-04-14 20:26:27 +0200

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   namespace :admin do
 		# callback for jQuery sort action
-    match 'taxons/reorder_products/:id' => 'taxons#reorder_products', :via => :put
+    match 'taxonomies/reorder_products/:id' => 'taxonomies#reorder_products', :via => :put
        
     resources :product_taxons do
       collection do
